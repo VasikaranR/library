686568c25850f000ceddc6ed49af5921d9ba4e20
VASIKARAN R <vasichan2000@gmail.com>
2022-06-15 22:48:47 +0530

    backend

---
+++ LibraryManagement/src/app/nodejs/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/library',(err)=>{
+    if(!err){
+        console.log('Mongose connection Succeeded..');
+    }
+    else{
+        console.log('error in db connection : '+ JSON.stringify(err,undefined,2));
+    }
+});
+module.exports=mongoose;
